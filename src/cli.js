eeb27d3219d583b41f5c803bec9e10609d965b8d
Daniel Han <daniel.han@klarna.com>
2019-05-02 16:59:23 +0200

    Pretty enough

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-require('./')()
+const yargs = require('yargs')
+const browserstackGuard = require('./')
+
+const args = yargs.option('username', {
+    alias: 'u',
+    default: process.env.BROWSERSTACK_USERNAME || '',
+    description: 'User name of your browserstack account'
+  })
+  .option('accesskey', {
+    alias: 'k',
+    default: process.env.BROWSERSTACK_ACCESS_KEY || '',
+    description: 'Access key of your browserstack account'
+  })
+  .option('limit', {
+    alias: 'l',
+    default: 5,
+    description: 'Number of parallel tests allowed in browerstack'
+  })
+  .argv
+
+browserstackGuard(args.username, args.accesskey, args.limit)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-const browserstackGuard = () => {
-  console.log('browserstackGuard >>> I am called')
+import axios from 'axios'
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+class Browserstack {
+  constructor(userName, accessKey, limit) {
+    axios.defaults.baseURL = 'https://api.browserstack.com'
+    axios.defaults.headers.common['Authorization'] = `Basic ${Buffer.from(`${userName}:${accessKey}`).toString('base64')}`
+  }
+  async getWorkers() {
+    const resp = await axios.get('/5/workers')
+    return resp.data
+  }
+  async waitUntilBelowLimit(limit) {
+    process.stdout.write('Checking available executor on Browserstack...')
+    while(true) {
+      const runningWorkers = await this.getWorkers()
+      const numberOfRunningWorkers = runningWorkers.filter(worker => worker.status === 'running').length
+      const numberOfQueuedWorkers = runningWorkers.filter(worker => worker.status === 'queue').length
+
+      if ( numberOfRunningWorkers < limit && numberOfQueuedWorkers == 0) {
+        console.log('done.')
+        break
+      }
+      process.stdout.write('.')
+      await sleep(1000)
+    }
+  }
 }
 
-module.exports = browserstackGuard
+module.exports = async (userName, accessKey, limit) => {
+  const browserstackGuard = new Browserstack(userName, accessKey)
+  await browserstackGuard.waitUntilBelowLimit(limit)
+}
